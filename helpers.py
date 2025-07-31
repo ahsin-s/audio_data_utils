c2c78f29bc66d48f28295809285ff4df2c03337e
Ahsin Shabbir <ashabbi00@gmail.com>
2025-07-30 23:08:47 -0400

    move the logic to handle either resampling or resizing into a class based approach with much cleaner code

--- helpers.py
+++ helpers.py
@@ -2,10 +2,12 @@ from typing import List
 from pathlib import Path 
 
 
-def find_files(root_directory, file_suffix) -> List[Path]:
+def find_files(root_directory, file_suffix, as_generator: bool=False) -> List[Path]:
     if not file_suffix.startswith("."):
         file_suffix = "." + file_suffix
     p = Path(root_directory)
-    globbed = list(p.glob(f"**/*{file_suffix}"))
+    globbed = p.glob(f"**/*{file_suffix}")
+    if not as_generator:
+        globbed = list(globbed)
 
     return globbed
--- normalized_audio_data.py
+++ normalized_audio_data.py
@@ -3,97 +3,160 @@ import asyncio
 import argparse
 from pathlib import Path 
 from more_itertools import chunked
+from typing import List, Dict, Any
 
 import numpy as np 
 import soundfile as sf
 import librosa 
 import tqdm
 
-from helpers import find_files 
+from helpers import find_files
 
-def chunk_audio_data(audio_data, samplerate, target_length, output_filepath, overlap_factor=0.1):
-    audio_size = len(audio_data) 
-    # chunk with some overlap 
-    chunk_counter = 0
-    chunked_output_paths = []
-    while len(audio_data) > target_length:
-        chunk = audio_data[:target_length]
+class AudioDataUtility:
+    def __init__(
+        self, 
+        source_directory: str, 
+        output_directory: str, 
+        file_extension: str,
+        target_length: int, 
+        target_samplerate: int,
+        padding_strategy: str = "repeat",
+        resume: bool = False,
+        show_progress_bar: bool = True,
+        concurrency_limit: int = 128,
+        ):
+        self.source_directory = source_directory
+        self.output_directory = output_directory
+        self.file_extension =  file_extension
+        self.target_length = target_length 
+        self.target_samplerate = target_samplerate
+        self.resume = resume 
+        self.show_progress_bar = show_progress_bar
+        self.concurrency_limit = concurrency_limit
 
-        # clip remaining with some overlap
-        audio_data = audio_data[int(target_length*(1-overlap_factor))::]
+        if not padding_strategy in ["repeat", "zero_pad"]:
+            raise ValueError("`padding_strategy` must be 'repeat' or 'zero_pad'")
 
-        # save the chunk
-        chunk_filename = Path(output_filepath).stem + "_" + str(chunk_counter)
-        chunk_output_path = Path(output_filepath).with_stem(chunk_filename)
-        sf.write(chunk_output_path, chunk, samplerate)
-        chunked_output_paths.append(str(chunk_output_path))
-        chunk_counter+=1
+        self.padding_strategy_func = {
+            "repeat": self.repeat_audio,
+            "zero_pad": self.zero_pad_audio
+        }[padding_strategy]
+
+        os.makedirs(output_directory, exist_ok=True)
+
+        print("Searching for files in the source directory (this can take some time)")
+        self.all_source_files = find_files(str(self.source_directory), file_extension, as_generator=True)
+        # print(f"Found {len(self.all_source_files)} files")
+
+        self.chunking_overlap_factor = 0.1
     
-    # The last chunk will possibly have more overlap
-    last_chunk = audio_data[-target_length::]
-    chunk_filename = Path(output_filepath).stem + "_" + str(chunk_counter)
-    chunk_output_path = Path(output_filepath).with_stem(chunk_filename)
-    sf.write(chunk_output_path, last_chunk, samplerate)
-    chunked_output_paths.append(chunk_output_path)
-    return chunked_output_paths
+    def set_chunking_overlap_factor(self, factor: float): 
+        self.chunking_overlap_factor = factor 
 
 
+    def chunk_audio_to_target_length(self, audio_data, target_length) -> List[np.array]:
+        audio_size = len(audio_data) 
+        # chunk with some overlap 
+        chunked_audio = []
+        while len(audio_data) > target_length:
+            chunk = audio_data[:target_length]
+            chunked_audio.append(chunk)
+            audio_data = audio_data[int(target_length*(1-self.chunking_overlap_factor))::]
 
 
+        last_chunk = audio_data[-target_length::]
+        chunked_audio.append(last_chunk)
+        return chunked_audio
+    
+    def repeat_audio(self, audio_data):
+        audio_size = len(audio_data)
+        repeat_factor = int(np.ceil(self.target_length / audio_size))
+        audio_data = np.tile(audio_data, repeat_factor)
+        return audio_data[:self.target_length]
 
-async def resize_audio(duration_seconds: int, expected_samplerate: int, input_filepath: str, output_filepath, semaphore):
-    async with semaphore:
-        try:
-            target_length = duration_seconds*expected_samplerate
-            x, sample_rate = sf.read(input_filepath)
-            if x.ndim > 1:
-                x = np.mean(x, axis=1)  # Convert to mono
+    def zero_pad_audio(self, audio_data):
+        zeros_needed = self.target_length - len(audio_data)
+        zeros_array = np.zeros(zeros_needed)
+        audio_data = np.concat([audio_data, zeros_array])
+        return audio_data
+
+    def save_chunked_audio(self, filename, chunked_audio, samplerate) -> List[str]:
+        output_filepaths = []
+        if len(chunked_audio) == 1:
+            # no need to modify the filename to include the chunk info 
+            output_filepath = os.path.join(self.output_directory, filename) 
+            sf.write(output_filepath, chunked_audio[0], samplerate)
+            output_filepaths.append(output_filepath)
+        else:
+            for chunknum, audiochunk in enumerate(chunked_audio):
+                # add the chunknum suffix to the original filename
+                chunk_filename = Path(filename).stem + f"_{chunknum}" + Path(filename).suffix
+                output_filepath = os.path.join(self.output_directory, chunk_filename) 
+                sf.write(output_filepath, audiochunk, samplerate)
+                output_filepaths.append(output_filepath)
+        return output_filepaths
+
+    def normalize_audio_size_maintain_samplerate(self):
+        rechunked_filepaths_lookup = {}
+        for filepath in self.maybe_show_progress(self.all_source_files):
+            try:
+                audio_data, samplerate = sf.read(filepath)
+                if len(audio_data) > self.target_length:
+                    # too short, need to apply padding or looping logic
+                    audio_data = self.padding_strategy_func(audio_data)
+                    chunked_audio = [audio_data]
+                else:
+                    # too long, need to chunk it while maintaining the same samplerate
+                    chunked_audio = self.chunk_audio_to_target_length(audio_data, samplerate)
                 
-            if sample_rate > expected_samplerate:
-                # in this case, resample to downsize the audio
-                print(f"{input_filepath} samplerate: {sample_rate} ; length: {len(x)}, total_duration: {len(x) / sample_rate}")
-                x = await asyncio.to_thread(librosa.resample, x, orig_sr=sample_rate, target_sr=expected_samplerate)
-            current_samples=len(x)
-            if current_samples < target_length: 
-                # apply the repeat logic
-                repeat_factor = int(np.ceil(target_length / current_samples))
-                x = np.tile(x, repeat_factor)
-                x = x[:target_length]  # additional validation
-                sf.write(output_filepath, x, expected_samplerate)
-                return output_filepath
-            else: 
-                # the audio file needs to be clipped to smaller length chunks
-                chunked_filepaths = chunk_audio_data(x, expected_samplerate, target_length, output_filepath)
-                return chunked_filepaths
-        except Exception as e:
-            print(e)
+                # save 
+                saved_to = self.save_chunked_audio(Path(filepath).name, chunked_audio, samplerate)
+                rechunked_filepaths_lookup[filepath] = saved_to
+            except Exception as e:
+                print(f"Couldn't process {filepath}")
+                print(e)
 
-async def run_resize_for_all_files_in_directory(source_directory, output_directory: str, file_suffix, concurrency_limit, duration: int, samplerate: int, resume: bool):
-    output_directory = Path(output_directory)
-    source_directory = Path(source_directory)
-    os.makedirs(output_directory, exist_ok=True)
+        print("Complete")
 
-    all_files = find_files(str(source_directory), file_suffix)
-    print(f"Found {len(all_files)} files")
-    
-    semaphore = asyncio.Semaphore(concurrency_limit)
-    for batch in tqdm.tqdm(chunked(all_files, 10000), total=len(all_files)//10000 + 1, desc="Processing"):
+    async def resize_audio(self, input_filepath, output_filepath, semaphore):
+        async with semaphore:
+            try:
+                x, sample_rate = sf.read(input_filepath)
+                if x.ndim > 1:
+                    x = np.mean(x, axis=1)  # Convert to mono
+                    
+                if sample_rate > self.target_samplerate:
+                    # in this case, resample to downsize the audio
+                    print(f"{input_filepath} samplerate: {sample_rate} ; length: {len(x)}, total_duration: {len(x) / sample_rate}")
+                    x = await asyncio.to_thread(librosa.resample, x, orig_sr=sample_rate, target_sr=self.target_samplerate)
+                current_samples=len(x)
+                if current_samples < self.target_length: 
+                    # too short - apply the padding logic
+                    x = self.padding_strategy_func(x)
+                x = x[:self.target_length]  # clips if the audio data is still too long
+                sf.write(output_filepath, x, self.target_samplerate)
+            except Exception as e:
+                print(e)
+
+    async def normalize_audio_resample_if_needed(self):
+        semaphore = asyncio.Semaphore(self.concurrency_limit)
         tasks = []
-        for source_file in batch:
-            relative_path = source_file.relative_to(source_directory)
-            output_filepath = output_directory.joinpath(relative_path)
-            if output_filepath.exists() and resume:
-                # skip
-                continue
-            os.makedirs(output_filepath.parent, exist_ok=True)
-            tasks.append(resize_audio(
-                duration,
-                samplerate,
-                str(source_file),
-                str(output_filepath),
-                semaphore
-            )) 
-        await asyncio.gather(*tasks)
+        for filepath in self.maybe_show_progress(self.all_source_files):
+            if len(tasks) >= 1000:
+                asyncio.gather(*tasks)
+                tasks = []
+            output_filepath = os.path.join(self.output_directory, Path(filepath).name)
+            if os.path.exists(output_filepath) and not self.resume:
+                # output path exists but resume is False so it will be overwritten
+                tasks.append(self.resize_audio(filepath, output_filepath)) 
+        # do any remaining tasks 
+        asyncio.gather(*tasks) 
+        print("Complete")
+        
+    def maybe_show_progress(self, iterable):
+        if self.show_progress_bar:
+            return tqdm.tqdm(iterable)
+        return iterable
 
 
 def get_args():
@@ -101,6 +164,8 @@ def get_args():
     parser.add_argument("--source_directory", type=str, required=True)
     parser.add_argument("--output_directory", type=str, required=True)
     parser.add_argument("--file_suffix", type=str, required=True)
+    parser.add_argument("--normalization_type", type=str, choices=["resample", "resize"], 
+        help="Choose 'resample' to modify the audio to a fixed sample rate with clipping, or 'resize' to maintain the sample rate while chunking the audio with overlap to a fixed size")
     parser.add_argument("--duration", type=int, required=True)
     parser.add_argument("--samplerate", type=int, required=True)
     parser.add_argument("--concurrency_limit", required=False, type=int, default=64)
@@ -109,12 +174,16 @@ def get_args():
 
 if __name__ == "__main__":
     args = get_args()
-    asyncio.run(run_resize_for_all_files_in_directory(
+    
+    target_length = args.duration * args.samplerate
+    audio_data_utility = AudioDataUtility(
         args.source_directory,
         args.output_directory,
         args.file_suffix,
-        args.concurrency_limit,
-        args.duration,
+        target_length,
         args.samplerate,
-        args.resume
-    ))
+    )
+    if args.normalization_type == "resample":
+        asyncio.run(audio_data_utility.normalize_audio_resample_if_needed())
+    else:
+        audio_data_utility.normalize_audio_size_maintain_samplerate()
